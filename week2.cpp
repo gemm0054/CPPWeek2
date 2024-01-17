efdd7a6737a9f7e040f564dbb451bc0b2484eaea
gemm0054 <gemm0054@algonquinlive.com>
2024-01-17 17:11:16 -0500

    Finished Constructors

--- week2.cpp
+++ week2.cpp
@@ -1,8 +1,27 @@
 
 #include<iostream>
 
+using namespace std;
+
+class Vehicle{ 
+	private:
+		int numWheels;
+		int numDoors;
+	public:
+		Vehicle(int w, int d) {
+			numWheels = w;
+			numDoors = d;
+		}
+		Vehicle(int w) {
+			Vehicle(w, 4);
+		}
+		Vehicle() {
+			Vehicle(4);
+		}
+};
+
 int main(int argc, char **argv)
 {
-   std::cout << "Hello World!" << std::endl;
+   cout << "Hello World!" << endl;
    return 0;
 }
