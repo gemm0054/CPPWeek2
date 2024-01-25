674606c568eab4bd37237932088806812e3b5349
gemm0054 <gemm0054@algonquinlive.com>
2024-01-24 19:04:43 -0500

    Finished Week3

--- week2.cpp
+++ week2.cpp
@@ -14,12 +14,10 @@ namespace CST8219 {
 			numDoors = d;
 			cout << "In constructor with 2 parameters" << endl;
 		}
-		Vehicle(int w) {
-			Vehicle(w, 4);
+		Vehicle(int w) : Vehicle(w, 4) {
 			cout << "In constructor with 1 parameters, wheels = " << w << endl;
 		}
-		Vehicle() {
-			Vehicle(4);
+		Vehicle() : Vehicle(4) {
 			cout << "In constructor with 0 parameters" << endl;
 		}
 		~Vehicle() {
@@ -30,7 +28,39 @@ namespace CST8219 {
 
 int main(int argc, char **argv)
 {
-	CST8219::Vehicle myVehicle(4, 2);
-	cout << "I made a Vehicle!" << endl;
+	CST8219::Vehicle veh1;
+	CST8219::Vehicle veh2(4);
+	CST8219::Vehicle veh3(4, 2);
+	cout << "Vehicle takes " << sizeof(veh1) << endl;
+	cout << "Vehicle takes " << sizeof(veh2) << endl;
+	cout << "Vehicle takes " << sizeof(veh3) << endl;
+
+	int w;
+	int d;
+	char c = 'a';
+	CST8219::Vehicle *pVehicle;
+	do {
+		cout << "enter number of doors" << endl;
+		if (!(cin >> d) || d <= 0) {
+			cout << "Enter a number greater than 0 for the doors" << endl;
+			cin.clear();
+			cin.ignore(256, '\n');
+			continue;
+		}
+		cin.ignore(256, '\n');
+		cout << "enter number of wheels" << endl;
+		if (!(cin >> w) || w <= 0) {
+			cout << "Enter a number greater than 0 for the wheels" << endl;
+			cin.clear();
+			cin.ignore(256, '\n');
+			continue;
+		}
+		pVehicle = new CST8219::Vehicle(w, d);
+		cout << "Enter 'q' or 'Q' to quit. Enter any other key to create another vehicle" << endl;
+		cin >> c;
+		cin.clear();
+		cin.ignore(256, '\n');
+		delete pVehicle;
+	} while (c != 'q' && c != 'Q');
 	return 0;
 }
